581bfcee559e8bd6f2ab6f4bea0dee8f64e66b7b
Noname794 <qn578389@gmail.com>
2025-03-19 08:17:01 +0700

    Noi dung hibernate 19_3

---
+++ hibernate_again/src/main/java/vn/begin/hibernate_again/App.java
@@ -0,0 +1,10 @@
+package vn.begin.hibernate_again;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
